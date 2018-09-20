fe836583a742760d4719e16d03e32afa13a76302
liluxin <liluxin@tpson.cn>
2018-09-20 19:38:43 +0800

    增加字典类型转换

---
+++ Map格式化/NSDictionary+LXExtention.h
@@ -0,0 +1,14 @@
+//
+//  NSDictionary+LXExtention.h
+//  com.tpson.community_firectl_guard
+//
+//  Created by 李lucy on 2018/6/29.
+//  Copyright © 2018年 tpson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (LXExtention)
++(id)changeType:(id)myObj;
+
+@end
